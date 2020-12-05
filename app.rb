b2f4d41f901ab0bea77e18ec40305c8b74722a1f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-12-05 02:23:22 +0100

    Add formatter with rubocop

--- app.rb
+++ app.rb
@@ -2,15 +2,14 @@ require 'google_drive'
 require 'csv'
 require 'pry'
 
-session = GoogleDrive::Session.from_config("config.json")
-index = '1nsBIvK4a_jdW4h4Wpd2S2KqJqTvyx3lFN5jeFJVDdH0'
+session = GoogleDrive::Session.from_config('config.json')
+INDEX_FILE = '1nsBIvK4a_jdW4h4Wpd2S2KqJqTvyx3lFN5jeFJVDdH0'.freeze
 
-# sheets = session.spreadsheet_by_key("1AregRaxaT_DTLGa2bvtuJhvId64KWn1PAREunZnM5v0").worksheets
-index = session.spreadsheet_by_key("1nsBIvK4a_jdW4h4Wpd2S2KqJqTvyx3lFN5jeFJVDdH0").worksheets[0]
+index = session.spreadsheet_by_key(INDEX_FILE).worksheets[0]
 
 chapters = index.rows.drop(1).first(21)
 chapters.each do |chapter|
-  number, title, _, _, _, _, _, key = chapter
+  _number, title, _, _, _, _, _, key = chapter
 
   next if key.empty?
 
@@ -19,8 +18,8 @@ chapters.each do |chapter|
   sheets.each do |sheet|
     rows = sheet.rows
     title = sheet.title.gsub('.', '-')
-    csv_str = rows.inject([]) { |csv, row|  csv << CSV.generate_line(row) }.join("")
+    csv_str = rows.inject([]) { |csv, row| csv << CSV.generate_line(row) }.join('')
     filename = "isteebu/isteebu-annuaire-2018-#{title}.csv"
-    File.open(filename, "w") {|f| f.write(rows.inject([]) { |csv, row|  csv << CSV.generate_line(row) }.join(""))}
+    File.open(filename, 'w') { |f| f.write(csv_str) }
   end
 end
