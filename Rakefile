4ac6739fbe4a3d6acfa1014dc9b6d3b0e8656fea
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-12-05 03:31:45 +0100

    Export in json for preview

---
+++ Rakefile
@@ -0,0 +1,9 @@
+task default: %w[test]
+
+task :dl do
+  ruby 'app.rb'
+end
+
+task :json do
+  ruby 'to_json.rb'
+end
--- to_json.rb
+++ to_json.rb
@@ -1,18 +1,20 @@
 require 'json'
 require 'csv'
+require 'pry'
 
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-filepath = 'isteebu/isteebu-annuaire-2018-2-02.csv'
 
-headers = CSV.read(filepath, headers: true).headers
+Dir.glob('isteebu/*.csv') do |filepath|
+  headers = CSV.read(filepath, headers: true).headers
 
-data = []
-CSV.foreach(filepath, csv_options) do |row|
-  line = {}
-  headers.each do |column|
-    line[column] = row[column]
+  data = []
+  CSV.foreach(filepath, csv_options) do |row|
+    line = {}
+    headers.each do |column|
+      line[column] = row[column]
+    end
+    data << line
   end
-  data << line
-end
 
-File.open("json/#{File.basename(filepath, '.csv')}.json", 'w') { |file| file.write(data.to_json) }
+  File.open("json/#{File.basename(filepath, '.csv')}.json", 'w') { |file| file.write(data.to_json) }
+end
