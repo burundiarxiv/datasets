d20d30adde89ec0c539223f8660af232cc7cf2b6
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-12-30 15:29:32 +0100

    Export data in fr and en

--- lib/home.rb
+++ lib/home.rb
@@ -7,27 +7,39 @@ require_relative '../index'
 BASE_URL = '/datasets'
 
 session = GoogleDrive::Session.from_config('config.json')
-sheet = session.spreadsheet_by_key(INDEX_FILE).worksheets[1]
-rows = sheet.rows
 
-datasets = {}
-rows.each do |row|
-  category = row[0]
-  id = row[1].gsub(/\./, '-').chop!
-  name = row[2]
-  path = "#{BASE_URL}/isteebu-annuaire-2018-#{id}.csv"
+def export_data(language, sheet)
+  rows = sheet.rows
 
-  data = { name: name, path: path, id: id }
+  datasets = {}
+  rows.each do |row|
+    category = row[0]
+    id = row[1].gsub(/\./, '-').chop!
+    name = row[2]
+    path = "#{BASE_URL}/isteebu-annuaire-2018-#{id}.csv"
 
-  datasets[category] = [] unless datasets.key?(category)
+    data = { name: name, path: path, id: id }
 
-  datasets[category] << data
+    datasets[category] = [] unless datasets.key?(category)
+
+    datasets[category] << data
+  end
+
+  datasetsv2 = datasets.map do |category, data|
+    { category: category, data: data.sort_by { |d| d[:name] } }
+  end
+
+  filename = "#{OPEN_DATA_PUBLIC_PATH}/#{language}/datasets.json"
+
+  File.open(filename, 'w') do |file|
+    file.write(datasetsv2.to_json)
+  end
 end
 
-datasetsv2 = datasets.map do |category, data|
-  { category: category, data: data.sort_by { |d| d[:name] } }
-end
+LOCALES.each do |locale|
+  language = locale[0]
+  index = locale[1]
 
-File.open(OPEN_DATA_HOME_ENDPOINT.to_s, 'w') do |file|
-  file.write(datasetsv2.to_json)
+  sheet = session.spreadsheet_by_key(INDEX_FILE).worksheets[index]
+  export_data(language, sheet)
 end
